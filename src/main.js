7b08dc7a0acfc243051137ca93592f8c3da94885
RamboWu <RamboWu90@gmail.com>
2017-09-25 14:22:58 +0800

    将 微信api整合到 electron中ing

--- src/main.js
+++ src/main.js
@@ -142,4 +142,21 @@ class ElectronicWeChat {
 
 }
 
-new ElectronicWeChat().init();
+//new ElectronicWeChat().init();
+
+const Weixinbot = require('weixinbot')
+
+// will send qrcode to your email address
+const bot = new Weixinbot()
+
+// will emit when bot fetch a new qrcodeUrl
+bot.on('qrcode', (qrcodeUrl) => {
+  console.log(qrcodeUrl)
+})
+
+bot.on('friend', (msg) => {
+  console.log(msg.Member.NickName + ': ' + msg.Content)
+  bot.sendText(msg.FromUserName, 'Got it')
+})
+
+bot.run()
